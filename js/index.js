db9d042b28c45776e51be121661cbc3cd147c0f3
Bibek Dahal <bibekdahal.bd16@gmail.com>
2020-05-18 00:37:15 -0500

    Complete the first design of landing page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function (e) {
     var nav = document.getElementsByTagName('nav').item(0);
-    if (document.documentElement.scrollTop > 1024) {
+    if (document.documentElement.scrollTop > 128) {
         nav.classList.add('scrolled');
     } else {
         nav.classList.remove('scrolled');
