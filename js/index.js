5c316b70802880f1dde24ad899d3005f216974bd
Bibek Dahal <bibekdahal.bd16@gmail.com>
2020-05-18 14:27:41 -0500

    Make mobile responsive

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function (e) {
     var nav = document.getElementsByTagName('nav').item(0);
-    if (document.documentElement.scrollTop > 128) {
+    if (document.documentElement.scrollTop > 96) {
         nav.classList.add('scrolled');
     } else {
         nav.classList.remove('scrolled');
