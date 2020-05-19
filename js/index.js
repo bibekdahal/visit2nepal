5ce71c9af5af5d9cff4a62dfa11a40c32cd970c9
Bibek Dahal <bibekdahal.bd16@gmail.com>
2020-05-19 02:03:31 -0500

    Make a basic search and filter page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function (e) {
     var nav = document.getElementsByTagName('nav').item(0);
-    if (document.documentElement.scrollTop > 96) {
+    if (document.documentElement.scrollTop > 32) {
         nav.classList.add('elevated');
     } else {
         nav.classList.remove('elevated');
